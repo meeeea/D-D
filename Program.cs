f09ddb84370c2c039df12cb77435955133585101
Justin Hutchens <Justinh22007@gmail.com>
2024-04-18 10:22:27 -0500

    ya

--- Program.cs
+++ Program.cs
@@ -1,5 +1,29 @@
-﻿class main {
-    public static void main() {
-        
+﻿using System.Collections;
+using System.Text;
+using System.Text.Unicode;
+
+class Program {
+    public static List<Book> BookSet = new();
+    public static void Main() {
+        OpenBookSet();
+    }
+
+    private static void OpenBookSet() {
+        DirectoryInfo d = new DirectoryInfo(".\\books"); //Assuming Test is your Folder
+
+        FileInfo[] Files = d.GetFiles("*.book"); //Getting Text files
+
+        foreach(FileInfo file in Files )
+        {
+            try {
+                BookSet.Add(new Book(file.Name));
+            }
+            catch (Book.Exceptions.IDException) {
+                Console.WriteLine($"Issue regarding the book ID of {file.Name}.");
+            }
+        }
+        foreach (Book book in BookSet) {
+            Console.WriteLine($"{book.Name}: {book.ID}");
+        }
     }
 }
